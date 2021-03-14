27fe53ea2b2df0cc2bc709f92fcae775242edd85
Ashley Edmondson <ace45611@creighton.edu>
2021-03-14 17:28:35 -0500

    Create cityWhile.js

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,15 @@
+let cities = []
+let i = 0
+let x = 0
+let doneWithCities = false
+
+while (doneWithCities == false) {
+    cities[i] = prompt("Enter a city name:")
+    i++
+    doneWithCities = confirm("Are you done entering cities?")
+}
+
+while (x < cities.length) {
+    console.log(cities[x].toLowerCase())
+    x++
+}
