25f570b431e786d0ab275f8f759c295f7148aeb6
Ashley Edmondson <ace45611@creighton.edu>
2021-03-14 17:39:34 -0500

    Create functionCompare.js

---
+++ forms/functionCompare/functionCompare.js
@@ -0,0 +1,14 @@
+//Creates a function that compares passwords entered by the user and determines if they are they same
+function compare(passOld, passNew){
+    let answer = pass1.localeCompare(pass2)
+    return Number(answer)
+}
+
+pass1 = prompt("Enter a password")
+pass2 = prompt("Enter a password")
+
+let newAnswer = compare(pass1,pass2)
+if (newAnswer == 0)
+    console.log("The passwords are the same.")
+else 
+    console.log("The passwords are not the same.")
