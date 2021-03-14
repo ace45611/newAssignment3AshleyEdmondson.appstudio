0342a7d9fdc001a7f8cdebc132ffce9f1c823632
Ashley Edmondson <ace45611@creighton.edu>
2021-03-14 17:34:53 -0500

    Create forReverseStates.js

---
+++ forms/forReverseStates/forReverseStates.js
@@ -0,0 +1,7 @@
+//Creates a for loop thats outputs the elements of an array BACKWARDS
+let states = ['Nebraska', 'Iowa', 'Minnesota', 'South Dakota', 'North Dakota', 'Kansas', 'Oklahoma', 'Texas']
+let y = ''
+
+for (y = states.length; y >= 0; y--) {
+    console.log(states[y])
+}
